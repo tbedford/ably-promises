c75b40f9234ad45b96bb484bbe1662a59fb5d7cf
Tony Bedford <tony.bedford@live.co.uk>
2022-07-19 16:51:43 +0100

    update

--- quickstart/index.js
+++ quickstart/index.js
@@ -1,9 +1,10 @@
-// Create the client
+// Include the promises interface
 const Ably = require('ably/promises');
+// Create the client
 const ably = new Ably.Realtime.Promise({ authUrl: 'https://ably.com/ably-auth/token/docs' });
 
+// wrapper for async functions
 const ablyRealtimePromiseExample = async () => {
-
     // Connect to Ably
     try {
         await ably.connection.once('connected');
@@ -16,8 +17,8 @@ const ablyRealtimePromiseExample = async () => {
     const channel = ably.channels.get('quickstart');
 
     // Subscribe to a channel
-    // the promise resolves when the channel is attached (and resolves synchronously
-    // if the channel is already attached)
+    // the promise resolves when the channel is attached
+    // (and resolves synchronously if the channel is already attached)
     await channel.subscribe('greeting', (message) => {
         console.log('Message is ==> '+ message.data)
     });
@@ -31,8 +32,9 @@ const ablyRealtimePromiseExample = async () => {
     setTimeout(() => {
         console.log("Closing connection...");
         ably.close();
-        console.log('closed');
+        console.log('Closed the connection to Ably.');
     }, 2000);    
 };
 
+// call wrapper
 ablyRealtimePromiseExample();
