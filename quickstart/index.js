a0b3929666ee9317a23f565a233396b7d7a22c68
Tony Bedford <tony.bedford@live.co.uk>
2022-07-19 14:02:22 +0100

    Initial commit

---
+++ quickstart/index.js
@@ -0,0 +1,31 @@
+const Ably = require('ably/promises');
+const client = new Ably.Realtime.Promise({ authUrl: 'https://ably.com/ably-auth/token/docs' });
+
+const ablyRealtimePromiseExample = async () => {
+
+    // Connect to Ably
+    try {
+        await client.connection.once('connected');
+        console.log('connected');
+    }
+    catch (error){
+        console.error(error);
+    }
+
+    // Subscribe to Ably
+    const channel = client.channels.get('myChannel');
+    await channel.subscribe('greeting', (message) => {
+        console.log('Message is ==> '+ message.data)
+    });
+
+    // Publish a message
+    await channel.publish('greeting', 'message 1');
+
+    /*
+    setTimeout(() => {
+        console.log("Goodbye World!");
+        client.close();
+    }, 5000);*/
+};
+
+ablyRealtimePromiseExample();
